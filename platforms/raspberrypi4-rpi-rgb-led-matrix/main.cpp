1f1d1984cf0055ab18ae7b7c1d40de2c40e9914b
Daniel Korgel <contact@danielkorgel.com>
2019-12-13 17:40:30 +0100

    Changed to gl2 header

--- platforms/raspberrypi4-rpi-rgb-led-matrix/main.cpp
+++ platforms/raspberrypi4-rpi-rgb-led-matrix/main.cpp
@@ -30,7 +30,7 @@
 #include <SDL2/SDL.h>
 #include <libconfig.h++>
 #include "bcm_host.h"
-#include "GLES/gl.h"
+#include "GLES2/gl2.h"
 #include "EGL/egl.h"
 #include "EGL/eglext.h"
 #include "gearboy.h"
